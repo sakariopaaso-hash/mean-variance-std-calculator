120a12eee7670801222333e5b6f46011d79f6b4b
sakariopaaso-hash <sakari.o.paaso@gmail.com>
2026-04-22 14:55:20 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from mean_var_std import calculate
+import unittest
+import test_module
+
+if __name__ == "__main__":
+    print(calculate([0,1,2,3,4,5,6,7,8]))
+    unittest.main(module='test_module', verbosity=2)
